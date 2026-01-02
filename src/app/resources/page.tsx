6f25581cd84c4bb382cf944311b0ed61f293a4b0
Hafiz2023 <adilamin374@gmail.com>
2026-01-02 14:04:44 +0500

    update

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import Link from 'next/link';
+
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import { Button } from '@/components/ui/button';
